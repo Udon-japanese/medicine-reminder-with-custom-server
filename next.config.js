c0b4bc08339a129a28d31a03df832015b82be6e1
Udon-japanese <odaniyoshito@gmail.com>
2024-03-25 16:52:39 +0900

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
+const headers = [
+  "Accept", "Accept-Version", "Content-Length",
+  "Content-MD5", "Content-Type", "Date", "X-Api-Version",
+  "X-CSRF-Token", "X-Requested-With",
+];
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {  key: 'Access-Control-Allow-Origin', value: `${process.env.NEXTAUTH_URL}` },
+          { key: "Access-Control-Allow-Methods", value: "GET,POST" },
+          { key: "Access-Control-Allow-Headers", value: headers.join(", ") }
+          ]
+      }
+    ];
+  },
   images: {
     remotePatterns: [
       {
